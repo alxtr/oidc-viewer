fd3f9981372a0adbdf2e7cace3fc687a3823b58c
alxtr <alxtr@users.noreply.github.com>
2025-11-24 22:25:05 -0500

    Add presets

--- src/MistCentauri.SimpleOidc/Pages/Login.cshtml.cs
+++ src/MistCentauri.SimpleOidc/Pages/Login.cshtml.cs
@@ -1,10 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MistCentauri.SimpleOidc.ViewModels;
 
 namespace MistCentauri.SimpleOidc.Pages;
 
 public class LoginModel : PageModel
 {
-    public void OnGet()
+    private readonly IConfiguration _configuration;
+    
+    public List<OidcPreset> Presets { get; private set; }
+
+    public LoginModel(IConfiguration configuration)
     {
+        _configuration = configuration;
+    }
+    
+    public IActionResult OnGet()
+    {
+        Presets = _configuration.GetSection("OidcPresets").Get<List<OidcPreset>>() ?? [];
+        return Page();
     }
 }
